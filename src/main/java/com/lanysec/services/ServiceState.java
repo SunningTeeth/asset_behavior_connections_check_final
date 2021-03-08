a3d50eab5fa7fc00c43bd755b04146752f7993c1
daijb <daijb00@outlook.com>
2021-03-08 11:34:14 +0800

    资产连接关系初步完成

---
+++ src/main/java/com/lanysec/services/ServiceState.java
@@ -0,0 +1,20 @@
+package com.lanysec.services;
+
+/**
+ * @author daijb
+ * @date 2021/3/8 10:50
+ */
+public enum ServiceState {
+    /**
+     * 启动中, 不可工作
+     */
+    Starting,
+    /**
+     * 启动完毕, 正常工作
+     */
+    Ready,
+    /**
+     * 已关闭
+     */
+    Stopped
+}
